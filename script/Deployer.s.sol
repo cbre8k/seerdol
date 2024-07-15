63a52fa6fe452ae808abdb04de1cd7196d0883fd
cgccld <tasibii.web3@gmail.com>
2024-07-15 10:18:09 +0700

    💡postCheck example

--- script/Deployer.s.sol
+++ script/Deployer.s.sol
@@ -6,23 +6,35 @@ import {Foo, FooU, FooT} from "../src/Foo.sol";
 
 contract Deployer is BaseMigrate {
     function run() external {
-        deployFoo();
-        deployFooU();
-        deployFooT();
+        Foo foo = Foo(deployFoo());
+        FooU fooU = FooU(deployFooU());
+        FooT fooT = FooT(deployFooT());
+        _postCheck(foo, fooU, fooT);
     }
 
-    function deployFoo() public broadcast {
-        deployContract("Foo.sol:Foo", abi.encode());
+    function deployFoo() public broadcast returns (address) {
+        return deployContract("Foo.sol:Foo", abi.encode());
     }
 
-    function deployFooU() public broadcast {
-        deployUUPSProxy("Foo.sol:FooU", abi.encodeCall(FooU.initialize, ()));
+    function deployFooU() public broadcast returns (address) {
+        return
+            deployUUPSProxy(
+                "Foo.sol:FooU",
+                abi.encodeCall(FooU.initialize, ())
+            );
     }
 
-    function deployFooT() public broadcast {
-        deployTransparentProxy(
-            "Foo.sol:FooT",
-            abi.encodeCall(FooT.initialize, ())
-        );
+    function deployFooT() public broadcast returns (address) {
+        return
+            deployTransparentProxy(
+                "Foo.sol:FooT",
+                abi.encodeCall(FooT.initialize, ())
+            );
+    }
+
+    function _postCheck(Foo foo_, FooU foou_, FooT foot_) internal pure {
+        vm.assertEq(foo_.foo(), "foo", "Post check failed");
+        vm.assertEq(foou_.foo(), "fooU", "Post check failed");
+        vm.assertEq(foot_.foo(), "fooT", "Post check failed");
     }
 }
